d8b8e8b8e063edba244efa3e8cda2c87a27c631f
Sterling Quinn <sterl1789@gmail.com>
2019-10-30 23:45:16 -0700

    maybe register is fixed with jwt?

--- app/src/main/java/edu/uw/tcss450/polkn/teamjerrysbearstcss450/LoginFragment.java
+++ app/src/main/java/edu/uw/tcss450/polkn/teamjerrysbearstcss450/LoginFragment.java
@@ -15,6 +15,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.uw.tcss450.polkn.teamjerrysbearstcss450.model.Credentials;
 
@@ -72,5 +76,56 @@ public class LoginFragment extends Fragment {
     private void onRegisterClicked() {
         Navigation.findNavController(getView()).
                 navigate(R.id.action_nav_fragment_login_to_nav_fragment_register);
+    }
+
+    /**
+     * Handle the setup of the UI before the HTTP call to the webservice.
+     */
+    private void handleLoginOnPre() {
+        getActivity().findViewById(R.id.layout_login_wait).setVisibility(View.VISIBLE);
+    }
+    /**
+     * Handle onPostExecute of the AsynceTask. The result from our webservice is
+     * a JSON formatted String. Parse it for success or failure.
+     * @param result the JSON formatted String response from the web service
+     */
+    private void handleLoginOnPost(String result) {
+        try {
+            JSONObject resultsJSON = new JSONObject(result);
+            boolean success =
+                    resultsJSON.getBoolean(
+                            getString(R.string.keys_json_login_success));
+            Log.d("results", resultsJSON.toString());
+            if (success) {
+                LoginFragmentDirections
+                        .ActionLoginFragmentToHomeActivity homeActivity =
+                        LoginFragmentDirections
+                                .actionLoginFragmentToHomeActivity(mCredentials);
+                homeActivity.setJwt(
+                        resultsJSON.getString(
+                                getString(R.string.keys_json_login_jwt)));
+                Navigation.findNavController(getView())
+                        .navigate(homeActivity);
+                return;
+            } else {
+                //Login was unsuccessful. Don’t switch fragments and
+                // inform the user
+                Log.d("no success branch", "oop");
+                ((TextView) getView().findViewById(R.id.field_login_email))
+                        .setError("Login Unsuccessful");
+            }
+            getActivity().findViewById(R.id.layout_login_wait)
+                    .setVisibility(View.GONE);
+        } catch (JSONException e) {
+            //It appears that the web service did not return a JSON formatted
+            //String or it did not have what we expected in it.
+            Log.e("JSON_PARSE_ERROR", result
+                    + System.lineSeparator()
+                    + e.getMessage());
+            getActivity().findViewById(R.id.layout_login_wait)
+                    .setVisibility(View.GONE);
+            ((TextView) getView().findViewById(R.id.field_login_email))
+                    .setError("Login Unsuccessful");
+        }
     }
 }
