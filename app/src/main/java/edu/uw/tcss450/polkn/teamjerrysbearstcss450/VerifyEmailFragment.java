a521890cbdedd2e130a051032b64facfecd1b7ee
Nadia Polk <nadia.polk1@gmail.com>
2019-11-08 15:16:50 -0800

    "Back to Login" added to email verification screen

--- app/src/main/java/edu/uw/tcss450/polkn/teamjerrysbearstcss450/VerifyEmailFragment.java
+++ app/src/main/java/edu/uw/tcss450/polkn/teamjerrysbearstcss450/VerifyEmailFragment.java
@@ -3,11 +3,15 @@ package edu.uw.tcss450.polkn.teamjerrysbearstcss450;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -26,6 +30,19 @@ public class VerifyEmailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_verify_email, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.textView_register_link)
+                .setOnClickListener(this::backToLogin);
+    }
+
+    private void backToLogin(final View theButton) {
+        Navigation.findNavController(getView()).
+                navigate(R.id.action_nav_verify_to_login);
     }
 
 }
