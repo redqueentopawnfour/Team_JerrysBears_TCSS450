aa23b01be9a186db8a9b3b97822a8751997ac899
Sterling Quinn <sterl1789@gmail.com>
2019-12-04 10:57:42 -0800

    chat fragment formatted

--- app/src/main/java/edu/uw/tcss450/polkn/teamjerrysbearstcss450/ui/Chat/MyChatSearchRecyclerViewAdapter.java
+++ app/src/main/java/edu/uw/tcss450/polkn/teamjerrysbearstcss450/ui/Chat/MyChatSearchRecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import edu.uw.tcss450.polkn.teamjerrysbearstcss450.R;
